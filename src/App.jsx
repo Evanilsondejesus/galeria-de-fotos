7cc662c0a9b81c257fd85b1ffeb1bc3debe0e653
Evanilson Almeida <evaevanilson@gmail.com>
2025-01-05 01:10:11 -0300

    elemento header feito

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import Header from './components/Header';
 
 function App() {
   
 
   return (
     <>
-
+<Header/>
 
     </>
   )
---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+
+export default function Header() {
+
+  
+  return (
+<header class="container">
+ 
+  <div class="row justify-content-center">
+    <div class="col-12 col-md-4">
+      <h1>Galeria de fotos</h1>
+    </div>
+  </div>
+
+
+
+</header>
+
+
+
+  );
+}
